f7c74d68a5b16092e72400ce65c980fdcbfe5a32
Conrado1212 <conrado1212@gmail.com>
2024-10-02 19:02:35 +0200

    check palindrome stage 5

--- app/app.js
+++ app/app.js
@@ -3,12 +3,15 @@ const btn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 if(btn){
     btn.addEventListener("click",()=>{
+        const replace = input.value.toLowerCase.replace(/[^a-z0-9]/g,"");
         if(input.value === ""){
             alert("Please input a value");
         }else if(input.value.length === 1){
             result.innerText = `${input.value} is a palindrome`;
-        }else if(input.value === [...input.value].reverse().join("")){
+        }else if(replace === [...replace].reverse().join("")){
             result.innerText = `${input.value} is a palindrome`;
+        }else{
+            result.innerText = `${input.value} is not a palindrome`;
         }
     })
 }
